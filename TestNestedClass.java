956b13362192eafc8628f5a645ae6bd92db62350
Farhan8314 <42551143+Farhan8314@users.noreply.github.com>
2018-09-09 12:09:28 -0400

    Add files via upload

---
+++ TestNestedClass.java
@@ -0,0 +1,12 @@
+package nestedclass;
+
+public class TestNestedClass {
+
+public static void main (String [] args) {
+OuterClass outerclass = new OuterClass();
+    outerclass.outerAMethodA();
+outerclass.innerA.innerAMethodA();
+
+
+}
+}
